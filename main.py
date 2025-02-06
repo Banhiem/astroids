5cc3b6bdab88c790a5d5cc102179a0afa804cec2
Banhiem <Pus4jc@gmail.com>
2025-02-05 16:43:54 -0800

    B: updated main and created gitignore

--- main.py
+++ main.py
@@ -2,10 +2,20 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
+    pygame.init
     print("'Starting asteroids!'")
-
+    print("Screen width: 1280")
+    print("Screen height: 720")
+    while True:
+        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
