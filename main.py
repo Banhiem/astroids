139c1f3f7309eedea7cce649e05cedf4c144dbe1
Banhiem <Pus4jc@gmail.com>
2025-02-11 19:55:49 -0800

    D: updated main, player and constants

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import pygame
 from constants import *
 from player import Player
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+updatable=pygame.sprite.Group()
+drawable=pygame.sprite.Group()
+Player.containers=(updatable,drawable)
 
 def main():
     pygame.init()
@@ -15,13 +18,16 @@ def main():
     Clock=pygame.time.Clock()
     player=Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
+        updatable.update(dt)
         screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.draw(screen)
+        for dra in drawable:
+            dra.draw(screen)
         pygame.display.flip()
         dt=Clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
