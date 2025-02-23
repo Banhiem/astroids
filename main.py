edfcea40931a9fd526e088272bc8b540f8293ec6
Banhiem <Pus4jc@gmail.com>
2025-02-22 22:58:16 -0800

    E: adding to git to work with VScode

--- main.py
+++ main.py
@@ -1,33 +1,53 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-updatable=pygame.sprite.Group()
-drawable=pygame.sprite.Group()
-Player.containers=(updatable,drawable)
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
-    print("'Starting asteroids!'")
-    print("Screen width: 1280")
-    print("Screen height: 720")
-    dt=0
-    Clock=pygame.time.Clock()
-    player=Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
-        updatable.update(dt)
-        screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        for dra in drawable:
-            dra.draw(screen)
+
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+               print("GAME OVER!")
+               sys.exit()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        dt=Clock.tick(60)/1000
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
+
